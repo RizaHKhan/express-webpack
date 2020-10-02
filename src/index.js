f2dca5225d981709507556df75923f28b661e71b
khanr <khanriza@gmail.com>
2020-10-02 12:13:24 -0400

    finalized setup per article

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import logMessage from './js/logger'
-import './css/style.css'
+import logMessage from "./js/logger";
+import "./css/style.css";
 
-logMessage('Welcome To Expack!')
+logMessage("Welcome To Expack!");
+
+if (typeof module.hot !== "undefined") {
+  module.hot.accept(); // eslint-disable-link no undef
+}
---
+++ webpack.prod.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].js",
+  },
+  target: "web",
+  devtools: "source-map",
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        // Transpile ES6-8 into ES5
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        // Loads the javascript into html template provided
+        // Entry point is set below in HtmlWebpackPlugin in Plugins
+        test: /\.html$/,
+        use: [{ loader: "html-loader", options: { minimize: true } }],
+      },
+      {
+        test: /\.jpg$/,
+        use: [{ loader: "url-loader" }],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/html/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};
